d9a68a881c9640a8dde888a92db3a4d9c51d4deb
Muukii <muukii.app@gmail.com>
2019-10-01 06:13:14 +0900

    Initial Commit

---
+++ Vine/Vine.h
@@ -0,0 +1,19 @@
+//
+//  Vine.h
+//  Vine
+//
+//  Created by muukii on 2019/10/01.
+//  Copyright © 2019 muukii. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Vine.
+FOUNDATION_EXPORT double VineVersionNumber;
+
+//! Project version string for Vine.
+FOUNDATION_EXPORT const unsigned char VineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Vine/PublicHeader.h>
+
+
